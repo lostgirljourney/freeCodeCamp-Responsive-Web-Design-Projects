c786408d46242a3e4200bc5df35c6febfbbc83dc
Falguni Sarkar <falgunisarkar526@gmail.com>
2021-05-07 16:34:47 +0530

    information added

--- Build a Personal Portfolio Webpage/script.js
+++ Build a Personal Portfolio Webpage/script.js
@@ -1,1 +1,4 @@
 const projectName = 'portfolio';
+document.getElementById("myButton").onclick = function () {
+    location.href = "https://drive.google.com/file/d/1D9PobYLHca2GoN6x1ZmmS8GI6SgKZukh/view?usp=sharing";
+};
